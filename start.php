4f7b74d329016944c90a86179454df715dcc4f85
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2015-09-22 12:04:51 +0300

    Initial commit

---
+++ start.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Notification tools
+ */
+
+elgg_register_event_handler('init', 'system', 'notification_tools_init');
+
+/**
+ * Initialize the plugin
+ */
+function notification_tools_init () {
+
+}
