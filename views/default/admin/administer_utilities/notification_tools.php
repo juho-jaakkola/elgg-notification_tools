1f0d706ef52edcf67710146e04fb19987bcbe1f0
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2015-11-09 17:13:32 +0200

    The bulk actions for personal and friend notifications are now usable

--- views/default/admin/administer_utilities/notification_tools.php
+++ views/default/admin/administer_utilities/notification_tools.php
@@ -14,21 +14,19 @@ $user_count = elgg_get_entities(array(
 
 /**
  * Personal notifications
- *
+ */
 echo elgg_view('admin/administer_utilities/enable_setting', array(
 	'setting' => 'personal',
 	'count' => $user_count,
 ));
-*/
 
 /**
  * Friend collection notifications
- *
+ */
 echo elgg_view('admin/administer_utilities/enable_setting', array(
 	'setting' => 'collection',
 	'count' => $user_count,
 ));
-*/
 
 /**
  * Get count of group memberships
